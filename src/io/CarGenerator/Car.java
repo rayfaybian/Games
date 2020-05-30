214a92f694e002fab2ee5894c0420accb5bb7851
RayFaybian <fabian.suppan@hotmail.com>
2020-05-30 11:24:38 +0200

    cargenerator finished
    user can generate a car
    usergenerated cars will be exported into a txt file including price, color, brand etc...
    
    update 1:
    
    - more brands and colors to choose from
    - added brand specific models (Audi A4, Opel Astra etc...)

--- src/io/CarGenerator/Car.java
+++ src/io/CarGenerator/Car.java
@@ -6,45 +6,111 @@ public class Car {
     Scanner input = new Scanner(System.in);
     Engine engine;
     String brand;
+    String model;
     String color;
 
-    public Car(String brand, Engine engine, String color) {
+    public Car(String brand, String model, Engine engine, String color) {
         this.brand = brand;
+        this.model = model;
         this.engine = engine;
         this.color = color;
     }
 
     @Override
     public String toString() {
-        return brand + ";" + color + ";" + engine.toString() + ";" + calculatePrice() + "\n";
+        return brand + ";" + model + ";" + color + ";" + engine.toString() + ";" + calculatePrice() + "\n";
     }
 
     public int calculatePrice() {
         int priceBrand = 0;
+        int priceModel = 0;
         int priceColor = 0;
 
         switch (brand) {
             case "Mercedes":
-                priceBrand = 50000;
+                priceBrand = 25000;
                 break;
             case "Audi":
-                priceBrand = 55000;
+                priceBrand = 40000;
                 break;
             case "BMW":
-                priceBrand = 60000;
+                priceBrand = 30000;
                 break;
+            case"Seat":
+                priceBrand = 5000;
+                break;
+            case"Skoda":
+                priceBrand = 10000;
+                break;
+            case"Opel":
+                priceBrand = 15000;
+                break;
+            case"Volkswagen":
+                priceBrand = 20000;
+        }
+        switch(model){
+            case "A Klasse":
+            case "A3":
+            case "3er":
+            case "Golf":
+                priceModel = 10000;
+                break;
+            case "C Klasse":
+            case "A4":
+            case "4er":
+            case "Passat":
+                priceModel = 15000;
+                break;
+            case "E Klasse":
+            case "A5":
+            case "5er":
+                priceModel = 20000;
+                break;
+            case "Ibiza":
+            case "Fabia":
+                priceModel = 2500;
+                break;
+            case "Leon":
+            case "Octavia":
+            case "Polo":
+                priceModel = 5000;
+                break;
+            case "Arteca":
+            case "Superb":
+                priceModel = 7500;
+                break;
+            case "Astra":
+                priceModel = 3000;
+                break;
+            case "Corsa":
+                priceModel = 6000;
+                break;
+            case "Insignia":
+                priceModel = 9000;
+                break;
+
+
         }
         switch (color) {
             case "Black":
-                priceColor = 5000;
+                priceColor = 2000;
                 break;
             case "White":
-                priceColor = 8000;
+                priceColor = 4000;
                 break;
             case "Red":
+                priceColor = 6000;
+                break;
+            case "Yellow":
+                priceColor = 8000;
+                break;
+            case "Green":
                 priceColor = 10000;
                 break;
+            case "Blue":
+                priceColor = 12000;
+                break;
         }
-        return priceBrand + priceColor + engine.calculatePrice();
+        return priceBrand + priceModel + priceColor + engine.calculatePrice();
     }
 }
--- src/io/CarGenerator/Main.java
+++ src/io/CarGenerator/Main.java
@@ -14,6 +14,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
 
         String brand;
+        String model;
         String color;
         int horsepower;
         Drivetype dt;
@@ -27,166 +28,422 @@ public class Main {
         while (repeat) {
 
             brand = setBrand(input);
+            model = setModel(input,brand);
             color = setColor(input);
-            horsepower = setHorsepower(input);
             dt = setDriveType(input);
+            horsepower = setHorsepower(input);
 
             Engine myEngine = new Engine(dt, horsepower);
-            Car myCar = new Car(brand,myEngine,color);
+            Car myCar = new Car(brand, model, myEngine, color);
 
             cars.add(myCar);
 
-            System.out.println("Do you want to generate another car? y/n");
+            System.out.println("Do you want to generate another car?");
+            System.out.println(" -y- Yes\n -n- No");
             char userinput = input.next().charAt(0);
             if (userinput == 'n') {
-            repeat = false;
+                repeat = false;
             }
         }
         File carFile = new File("C:\\Coding\\Games\\src\\io\\files\\carFile.txt");
         FileWriter myFileWriter = new FileWriter(carFile);
-        myFileWriter.write("brand;color;drivetype;horsepower;price\n");
-        System.out.println("brand;color;drivetype;horsepower;price");
-        for (Car car: cars) {
+        myFileWriter.write("brand;model;color;drivetype;horsepower;price\n");
+        System.out.println("brand;model;color;drivetype;horsepower;price");
+        for (Car car : cars) {
             System.out.print(car.toString());
             myFileWriter.write(car.toString());
-
         }
         myFileWriter.close();
     }
 
-    public static Drivetype setDriveType(Scanner input) {
-        System.out.println("Choose Drivetype");
-        System.out.println(" -1- Gas\n -2- Diesel\n -3- Electric");
 
-        Drivetype dt = null;
-        try {
+    public static String setBrand(Scanner input) {
+        boolean brandSet = false;
+        String brand = "";
 
-            int userinput = input.nextInt();
-            switch (userinput) {
-                case 1:
-                    dt = GAS;
+        System.out.println("Welcome to the car generator.");
 
-                    break;
-                case 2:
-                    dt = DIESEL;
+        System.out.println("Pick a brand");
 
-                    break;
-                case 3:
-                    dt = ELECTRIC;
+        while (!brandSet) {
+            System.out.println(" -1- Mercedes\n -2- Audi\n -3- BMW\n -4- Seat\n -5- Skoda\n -6- Opel\n" +
+                    " -7- Volkswagen\n");
 
-                    break;
-                default:
-                    System.out.println("Please select 1,2 or 3");
+            try {
 
+                int userinput = input.nextInt();
+                switch (userinput) {
+                    case 1:
+                        brand = "Mercedes";
+                        brandSet = true;
+                        break;
+                    case 2:
+                        brand = "Audi";
+                        brandSet = true;
+                        break;
+                    case 3:
+                        brand = "BMW";
+                        brandSet = true;
+                        break;
+                    case 4:
+                        brand = "Seat";
+                        brandSet = true;
+                        break;
+                    case 5:
+                        brand = "Skoda";
+                        brandSet = true;
+                        break;
+                    case 6:
+                        brand = "Opel";
+                        brandSet = true;
+                        break;
+                    case 7:
+                        brand = "Volkswagen";
+                        brandSet = true;
+                        break;
+                    default:
+                        System.out.println("\n+++++ Please select 1,2 or 3 +++++");
+                        brandSet = false;
+                }
+            } catch (
+                    Exception e) {
+                System.out.println("Wrong input. Exit Program.");
+                System.exit(0);
             }
-        } catch (Exception e) {
-            System.out.println("Wrong input. Exit Program.");
-            System.exit(0);
+        }
+
+        return brand;
+    }
+
+    public static String setModel(Scanner input, String brand) {
+        String model = "";
+        boolean modelSet = false;
+
+        System.out.println("Pick a model");
+
+        while (!modelSet) {
+            if (brand.equals("Mercedes")) {
+                System.out.println(" -1- A Klasse\n -2- C Klasse\n -3- E Klasse");
+
+                try {
+
+                    int userinput = input.nextInt();
+                    switch (userinput) {
+                        case 1:
+                            model = "A Klasse";
+                            modelSet = true;
+                            break;
+                        case 2:
+                            model = "C Klasse";
+                            modelSet = true;
+                            break;
+                        case 3:
+                            model = "E Klasse";
+                            modelSet = true;
+                            break;
+                        default:
+                            System.out.println("\n+++++ Please select 1,2 or 3 +++++");
+                            modelSet = false;
+                    }
+                } catch (Exception e) {
+                    System.out.println("Wrong input. Exit Program.");
+                    System.exit(0);
+                }
+            }
+            if (brand.equals("Audi")) {
+                System.out.println(" -1- A3\n -2- A4\n -3- A5");
+
+                try {
+
+                    int userinput = input.nextInt();
+                    switch (userinput) {
+                        case 1:
+                            model = "A3";
+                            modelSet = true;
+                            break;
+                        case 2:
+                            model = "A4";
+                            modelSet = true;
+                            break;
+                        case 3:
+                            model = "A5";
+                            modelSet = true;
+                            break;
+                        default:
+                            System.out.println("\n+++++ Please select 1,2 or 3 +++++");
+                            modelSet = false;
+                    }
+                } catch (Exception e) {
+                    System.out.println("Wrong input. Exit Program.");
+                    System.exit(0);
+                }
+            }
+            if (brand.equals("BMW")) {
+                System.out.println(" -1- 3er\n -2- 4er\n -3- 5er");
+
+                try {
+
+                    int userinput = input.nextInt();
+                    switch (userinput) {
+                        case 1:
+                            model = "3er";
+                            modelSet = true;
+                            break;
+                        case 2:
+                            model = "4er";
+                            modelSet = true;
+                            break;
+                        case 3:
+                            model = "5er";
+                            modelSet = true;
+                            break;
+                        default:
+                            System.out.println("\n+++++ Please select 1,2 or 3 +++++");
+                            modelSet = false;
+                    }
+                } catch (Exception e) {
+                    System.out.println("Wrong input. Exit Program.");
+                    System.exit(0);
+                }
+            }
+            if (brand.equals("Seat")) {
+                System.out.println(" -1- Ibiza\n -2- Leon\n -3- Ateca");
+
+                try {
+
+                    int userinput = input.nextInt();
+                    switch (userinput) {
+                        case 1:
+                            model = "Ibiza";
+                            modelSet = true;
+                            break;
+                        case 2:
+                            model = "Leon";
+                            modelSet = true;
+                            break;
+                        case 3:
+                            model = "Ateca";
+                            modelSet = true;
+                            break;
+                        default:
+                            System.out.println("\n+++++ Please select 1,2 or 3 +++++");
+                            modelSet = false;
+                    }
+                } catch (Exception e) {
+                    System.out.println("Wrong input. Exit Program.");
+                    System.exit(0);
+                }
+            }
+            if (brand.equals("Skoda")) {
+                System.out.println(" -1- Fabia\n -2- Octavia\n -3- Superb");
+
+                try {
+
+                    int userinput = input.nextInt();
+                    switch (userinput) {
+                        case 1:
+                            model = "Fabia";
+                            modelSet = true;
+                            break;
+                        case 2:
+                            model = "Otavia";
+                            modelSet = true;
+                            break;
+                        case 3:
+                            model = "Superb";
+                            modelSet = true;
+                            break;
+                        default:
+                            System.out.println("\n+++++ Please select 1,2 or 3 +++++");
+                            modelSet = false;
+                    }
+                } catch (Exception e) {
+                    System.out.println("Wrong input. Exit Program.");
+                    System.exit(0);
+                }
+            }
+            if (brand.equals("Opel")) {
+                System.out.println(" -1- Astra\n -2- Corsa\n -3- Insignia");
+
+                try {
+
+                    int userinput = input.nextInt();
+                    switch (userinput) {
+                        case 1:
+                            model = "Astra";
+                            modelSet = true;
+                            break;
+                        case 2:
+                            model = "Corsa";
+                            modelSet = true;
+                            break;
+                        case 3:
+                            model = "Insignia";
+                            modelSet = true;
+                            break;
+                        default:
+                            System.out.println("\n+++++ Please select 1,2 or 3 +++++");
+                            modelSet = false;
+                    }
+                } catch (Exception e) {
+                    System.out.println("Wrong input. Exit Program.");
+                    System.exit(0);
+                }
+            }
+            if (brand.equals("Volkswagen")) {
+                System.out.println(" -1- Polo\n -2- Golf\n -3- Passat");
+
+                try {
+
+                    int userinput = input.nextInt();
+                    switch (userinput) {
+                        case 1:
+                            model = "Polo";
+                            modelSet = true;
+                            break;
+                        case 2:
+                            model = "Golf";
+                            modelSet = true;
+                            break;
+                        case 3:
+                            model = "Passat";
+                            modelSet = true;
+                            break;
+                        default:
+                            System.out.println("\n+++++ Please select 1,2 or 3 +++++");
+                            modelSet = false;
+                    }
+                } catch (Exception e) {
+                    System.out.println("Wrong input. Exit Program.");
+                    System.exit(0);
+                }
+            }
+        }
+        return model;
+    }
+
+    public static String setColor(Scanner input) {
+        String color = "";
+        boolean colorSet = false;
+
+        System.out.println("Pick a color");
+
+        while (!colorSet) {
+            System.out.println(" -1- Black\n -2- White\n -3- Red\n -4- Yellow\n -5- Green\n -6- Blue\n");
+
+
+            try {
+
+                int userinput = input.nextInt();
+                switch (userinput) {
+                    case 1:
+                        color = "Black";
+                        colorSet = true;
+                        break;
+                    case 2:
+                        color = "White";
+                        colorSet = true;
+                        break;
+                    case 3:
+                        color = "Red";
+                        colorSet = true;
+                        break;
+                    case 4:
+                        color = "Yellow";
+                        colorSet = true;
+                        break;
+                    case 5:
+                        color = "Green";
+                        colorSet = true;
+                        break;
+                    case 6:
+                        color = "Blue";
+                        colorSet = true;
+                        break;
+                    default:
+                        System.out.println("\n+++++ Please select 1,2 or 3 +++++");
+                        colorSet = false;
+                }
+            } catch (Exception e) {
+                System.out.println("Wrong input. Exit Program.");
+                System.exit(0);
+            }
+        }
+        return color;
+    }
+
+    public static Drivetype setDriveType(Scanner input) {
+        Drivetype dt = null;
+        boolean dtSet = false;
+        System.out.println("Choose Drivetype");
+        while (!dtSet) {
+            System.out.println(" -1- Gas\n -2- Diesel\n -3- Electric");
+
+
+            try {
+
+                int userinput = input.nextInt();
+                switch (userinput) {
+                    case 1:
+                        dt = GAS;
+                        dtSet = true;
+                        break;
+                    case 2:
+                        dt = DIESEL;
+                        dtSet = true;
+                        break;
+                    case 3:
+                        dt = ELECTRIC;
+                        dtSet = true;
+                        break;
+                    default:
+                        System.out.println("Please select 1,2 or 3");
+                        dtSet = false;
+                }
+            } catch (Exception e) {
+                System.out.println("\n+++++ Please select 1,2 or 3 +++++");
+                System.exit(0);
+            }
         }
         return dt;
     }
 
-
     public static int setHorsepower(Scanner input) {
-        System.out.println("Choose horsepower");
-        System.out.println(" -1- 100\n -2- 150\n -3- 200");
-
         int horsepower = 0;
-        try {
+        boolean horsepowerSet = false;
 
-            int userinput = input.nextInt();
-            switch (userinput) {
-                case 1:
-                    horsepower = 100;
+        System.out.println("Choose horsepower");
 
-                    break;
-                case 2:
-                    horsepower = 150;
+        while (!horsepowerSet) {
+            System.out.println(" -1- 100\n -2- 150\n -3- 200");
 
-                    break;
-                case 3:
-                    horsepower = 200;
 
-                    break;
-                default:
-                    System.out.println("Please select 1,2 or 3");
+            try {
 
+                int userinput = input.nextInt();
+                switch (userinput) {
+                    case 1:
+                        horsepower = 100;
+                        horsepowerSet = true;
+                        break;
+                    case 2:
+                        horsepower = 150;
+                        horsepowerSet = true;
+                        break;
+                    case 3:
+                        horsepower = 200;
+                        horsepowerSet = true;
+                        break;
+                    default:
+                        System.out.println("\n+++++ Please select 1,2 or 3 +++++");
+                        horsepowerSet = false;
+                }
+            } catch (Exception e) {
+                System.out.println("Wrong input. Exit Program.");
+                System.exit(0);
             }
-        } catch (Exception e) {
-            System.out.println("Wrong input. Exit Program.");
-            System.exit(0);
         }
         return horsepower;
-    }
-
-
-    public static String setColor(Scanner input) {
-        System.out.println("Pick a color");
-        System.out.println(" -1- Black\n -2- White\n -3- Red");
-
-        String color = "";
-        try {
-
-            int userinput = input.nextInt();
-            switch (userinput) {
-                case 1:
-                    color = "Black";
-
-                    break;
-                case 2:
-                    color = "White";
-
-                    break;
-                case 3:
-                    color = "Red";
-
-                    break;
-                default:
-                    System.out.println("Please select 1,2 or 3");
-
-            }
-        } catch (Exception e) {
-            System.out.println("Wrong input. Exit Program.");
-            System.exit(0);
-        }
-
-        return color;
-    }
-
-
-    public static String setBrand(Scanner input) {
-
-        System.out.println("Welcome to the car generator.");
-        System.out.println("Pick a brand");
-        System.out.println(" -1- Mercedes\n -2- Audi\n -3- BMW\n");
-
-        String brand = "";
-        try {
-
-            int userinput = input.nextInt();
-            switch (userinput) {
-                case 1:
-                    brand = "Mercedes";
-
-                    break;
-                case 2:
-                    brand = "Audi";
-
-                    break;
-                case 3:
-                    brand = "BMW";
-
-                    break;
-                default:
-                    System.out.println("Please select 1,2 or 3");
-
-            }
-        } catch (Exception e) {
-            System.out.println("Wrong input. Exit Program.");
-            System.exit(0);
-        }
-
-        return brand;
     }
 }
 
